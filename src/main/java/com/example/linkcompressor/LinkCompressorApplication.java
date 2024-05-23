981f9a2844a98fadc2b3dbdf65da66a973e07276
JeremiJL <j3remilipniacki@gmail.com>
2024-05-23 11:35:54 +0200

    initial commit

---
+++ src/main/java/com/example/linkcompressor/LinkCompressorApplication.java
@@ -0,0 +1,13 @@
+package com.example.linkcompressor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkCompressorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkCompressorApplication.class, args);
+    }
+
+}
